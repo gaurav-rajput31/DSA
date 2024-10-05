f933332b9fa550fb2ce0374733c34b091f264c85
Gaurav Rajput <139263972+gaurav-rajput31@users.noreply.github.com>
2024-10-06 00:09:08 +0530

    Update traversing_of_array.c

--- traversing_of_array.c
+++ traversing_of_array.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    // Initialize an array
-    int arr[] = {10, 20, 30, 40, 50};
-    int n = sizeof(arr) / sizeof(arr[0]);  // Calculate number of elements in array
+    int size;
 
-    // Traverse the array
-    for (int i = 0; i < n; i++) {
-        printf("Element at index %d: %d\n", i, arr[i]);
+    // declare size by user 
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+
+    int arr[size];
+
+    //loop to input array index data by user 
+    for (int y = 0; y < size; y++) {
+        printf("Enter element at index %d: ", y);
+        scanf("%d", &arr[y]);  
+    }
+
+    // Loop to print the elements of the array 
+    for (int y = 0; y < size; y++) {
+        printf("Element at index %d: %d\n", y, arr[y]);
     }
 
     return 0;
 }
-
